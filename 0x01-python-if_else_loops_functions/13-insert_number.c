46bc81aec27f7d5d10f63db4e09a87116e61acf0
TechByDami <agboolaolawale001@gmail.com>
2022-12-15 23:43:39 -0800

    bug fixed :earth_africa::trophy::nigeria::fire:

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,42 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
-
 /**
- * insert_node - inserts a number into a sorted singly linked list.
- * @head: list head
- * @number: number to store in the new node
- * Return: pointer to the new node
+ * instert_node - inserts in ordered list
+ * @head: head of list
+ * @number: number to put in
+ * Return: address of new node
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-        listint_t *runner;
-        listint_t *new;
+	listint_t *tmp,*new;
 
-        runner = *head;
-
-        new = malloc(sizeof(listint_t));
-        if (new == NULL)
-                return (NULL);
-        new->n = number;
-
-        if (*head == NULL || (*head)->n > number)
-        {
-                new->next = *head;
-                *head = new;
-                return (new);
-        }
-
-        while (runner->next != NULL)
-        {
-                if ((runner->next)->n >= number)
-                {
-                        new->next = runner->next;
-                        runner->next = new;
-                        return (new);
-                }
-                runner = runner->next;
-        }
-
-        new->next = NULL;
-        runner->next = new;
-        return (new);
+	tmp = *head;
+	new = malloc(sizeof(listint_t));
+	if(new == NULL)
+		return(NULL);
+	new->n = number;
+	new->next = NULL;
+	if((*head) == NULL)
+	{
+		*head = new;
+		return(new);
+	}
+	else if((*head)->n >= number)
+	{
+		new->next = *head;
+		*head = new;
+		return(new);
+	}
+	else
+	{
+		while(tmp->next != NULL)
+		{
+			if(tmp->next->n >= number)
+			{
+				new->next = tmp->next;
+				tmp->next = new;
+				return (new);
+			}
+			tmp = tmp->next;
+		}
+		new->next = NULL;
+		tmp->next = new;
+		return(new);
+	}
+	return(NULL);
 }
